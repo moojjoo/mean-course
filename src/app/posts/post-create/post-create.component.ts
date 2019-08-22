a205b5a39e7c434b51f1a5a93290a9c7694b1107
Robert B. Dannelly, Jr <robertdannelly@gmail.com>
2019-08-22 15:23:44 -0400

    mean-course update source

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import { post } from 'selenium-webdriver/http';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent{
+  enteredValue = '';
+  newPost = 'NO CONTENT';
+
+  onAddPost(postInput: HTMLTextAreaElement){
+    this.newPost = this.enteredValue;
+  }
+}
